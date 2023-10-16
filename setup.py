af33f60dcbb1078488b6b43326dccde2bfd1dd6d
BaptisteMadery14 <baptiste.madery@eomys.com>
2023-10-16 14:54:31 +0200

    wip

--- setup.py
+++ setup.py
@@ -14,13 +14,13 @@ from setuptools.command.build_ext import build_ext as _build_ext
 class build_ext(_build_ext):
     """build class that includes numpy directory"""
 
-    # def finalize_options(self):
-    #     _build_ext.finalize_options(self)
-    #     # Prevent numpy from thinking it is still in its setup process:
-    #     __builtins__.__NUMPY_SETUP__ = False
-    #     import numpy
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        # __builtins__.__NUMPY_SETUP__ = False
+        import numpy
 
-    #     self.include_dirs.append(numpy.get_include())
+        self.include_dirs.append(numpy.get_include())
 
     def build_extensions(self):
         if os.name != "nt":
